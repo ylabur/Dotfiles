5974143d553b3859925a4c0aea05c902c8dd8dbe
Yuta Labur <ylabur@linkedin.com>
2015-09-29 00:37:54 -0700

    added some vim-plug goodness. TODO still need to figure out best way to auto-run PlugInstall silently

--- install.sh
+++ install.sh
@@ -47,5 +47,13 @@ function vimdirs() {
   fi
   done
 }
+
+function vimplug() {
+	curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+	  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	  vim -c PlugInstall
+}
+
 dotfiles
 vimdirs
+vimplug
